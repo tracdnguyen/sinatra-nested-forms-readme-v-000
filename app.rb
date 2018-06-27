e691ad30118809a3f264f46e65fa8399611ef024
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-27 19:14:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,19 @@ require_relative 'models/course.rb'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :new
+  end
+
+  post '/student' do
+  @student = Student.new(params[:student])
+ 
+  params[:student][:courses].each do |details|
+    Course.new(details)
+  end
+ 
+  @courses = Course.all
+ 
+  erb :student
+end
 end
